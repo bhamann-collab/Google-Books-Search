6f4946d65b8bf819bd535fc8e81890df7ca063cf
bhamann-collab <brockohamann@gmail.com>
2020-07-18 14:46:36 +0930

    Starting with the autocomplete functionality

--- client/src/components/Search/SearchBar.js
+++ client/src/components/Search/SearchBar.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Container, Row, Col, Form, Label, Input, Button } from 'reactstrap';
+import axios from 'axios'
 
 const SearchBar = () => {
+    const [display, setDisplay] = useState(false);
+    const [options, setOptions] = useState([]);
+    const [search, setSearch] = useState("")
+
+    useEffect(() => {
+        if (search) {
+            let book = [];
+            axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+            .then(res => {
+                book = res.data.items.map(x => x.volumeInfo.title)
+                //book.push(...res.data.items)
+                console.log(book)
+            })
+        }
+    }, [search])
+
+    const inputChange = (e) => {
+        setSearch(e.currentTarget.value);
+    }
+
     return (
         <Container>
             <Row>
@@ -11,7 +32,7 @@ const SearchBar = () => {
                         <br />
                         <Form>
                             <Label for="Book">Book</Label>
-                            <Input type="Book" name="Book" placeholder="Search Book" id="bookValue"/>
+                            <Input type="Book" name="Book" onChange={inputChange} placeholder="Search Book" id="bookValue"/>
                             <div className="buttonContainer">
                                 <Button className="mt-3">Search</Button>
                             </div>
