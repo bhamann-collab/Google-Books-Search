d55cb80e29341c015b36efbac26e83f6688bc17c
bhamann-collab <brockohamann@gmail.com>
2020-07-20 11:21:07 +0930

    initial server & mongoose setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//Mongoose
+//---------------------------------
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/books', {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+//---------------------------------
+
+app.listen(PORT, err => {
+    if (err) throw err
+    console.log(`server started on port: ${PORT}`)
+})
