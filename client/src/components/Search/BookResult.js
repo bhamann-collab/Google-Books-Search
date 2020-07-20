24d6d08a95dc9ade861d10e15e9df28e486ca7fb
bhamann-collab <brockohamann@gmail.com>
2020-07-20 10:43:07 +0930

    CSS improvementrrs

--- client/src/components/Search/BookResult.js
+++ client/src/components/Search/BookResult.js
@@ -10,14 +10,15 @@ const BookResult = (props) => {
                         <Container fluid>
                             <Row>
                                 <Col xs="12">
-                                    <h3 className="text-center">{props.title}</h3>
-                                    <p className="text-center">Written By {props.authors}</p>
+                                    <h3 className="text-center textBold">{props.title}</h3>
+                                    <p className="text-center textBold">Written By {props.authors}</p>
                                 </Col>
-                                {/* <Col xs="6"></Col> */}
                             </Row>
                             <Row>
                                 <Col xs="3">
-                                    <img src={props.image} alt="book photo"/>
+                                    <div className="centerImg">
+                                        <img src={props.image} alt="book photo"/>
+                                    </div>
                                 </Col>
                                 <Col xs="9">
                                     <p>{props.description ? props.description : "description unavaliable"}</p>
