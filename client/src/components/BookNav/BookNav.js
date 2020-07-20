c430810fc76ae891726da3664c15e53259659015
bhamann-collab <brockohamann@gmail.com>
2020-07-20 23:31:29 +0930

    Routing works between the two pages

--- client/src/components/BookNav/BookNav.js
+++ client/src/components/BookNav/BookNav.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom'
 import './BookNav.css'
 
 const BookNav = () => {
     return (
         <div>
             <Nav className="whitenText">
-                <NavLink href="#">Google Books</NavLink> <NavLink href="#">Search</NavLink> <NavLink href="#">Saved</NavLink>
+                <NavLink href="#">Google Books</NavLink>
+                <Link to="/">
+                    <NavLink href="/">Search</NavLink>
+                </Link>
+                <Link to="/Saved">
+                    <NavLink href="/Saved">Saved</NavLink>
+                </Link> 
             </Nav>
             <hr />
         </div>
