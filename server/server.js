1dfef5eb6d92cfa6732bdf98331ce7208a4e8286
bhamann-collab <brockohamann@gmail.com>
2020-07-22 01:14:58 +0930

    setting up to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require("http")
-const socketIo = require("socket.io")
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require('path');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -39,6 +41,15 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/books', {
     useNewUrlParser: true, useUnifiedTopology: true
 });
 //---------------------------------
+
+//For Production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //Middleware
 app.use(cors())
